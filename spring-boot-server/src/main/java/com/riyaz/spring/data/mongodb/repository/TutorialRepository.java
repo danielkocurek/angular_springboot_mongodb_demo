5a2f6736b14a2c4c3609f1c8c3defbcca6c1c804
danielkocurek <daniel.kocurek212@gmail.com>
2023-10-04 17:18:01 -0500

    Initial commit

---
+++ spring-boot-server/src/main/java/com/riyaz/spring/data/mongodb/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.riyaz.spring.data.mongodb.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.riyaz.spring.data.mongodb.model.Tutorial;
+
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+}
